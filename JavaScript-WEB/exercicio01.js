8593b3b424a42afa59cbfc54eb5270e89844fb6d
Rafael Ferreira <Raffaferreira@users.noreply.github.com>
2015-09-09 16:40:50 -0300

    Update exercicio01.js

--- JavaScript-WEB/exercicio01.js
+++ JavaScript-WEB/exercicio01.js
@@ -3,5 +3,5 @@ var idade = prompt("Digite sua idade");
 var altura = prompt("Digite sua altura");
 var peso = prompt("Digite seu peso");
 var imc = (peso/(altura*2));
-
+//Exercicio 05
 alert("Seu nome é "+ nome +" você tem "+ idade +" anos e seu IMC é de "+ imc);
